288ab7cd1b97c2781803326bb1848d8e0757e2f0
Dostonbek <57319534+doston12@users.noreply.github.com>
2019-12-10 17:10:23 +0500

    Update DispatcherController.java

--- src/main/java/sample/inteface/DispatcherController.java
+++ src/main/java/sample/inteface/DispatcherController.java
@@ -6,9 +6,6 @@ import javafx.collections.ObservableList;
 import javafx.stage.Stage;
 import sample.controller.CreateProductTypeController;
 
-/**
- * Shoh Jahon tomonidan 6/28/2019 da qo'shilgan.
- */
 public interface DispatcherController<T> {
 
     void setStage(Stage stage);
